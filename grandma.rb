7251edcf0c3e699bbbd3281f3b982630a4d3247a
David Barbour <danceguy303@gmail.com>
2020-11-10 20:21:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 
-def speak_to_grandma
-  if "I LOVE YOU GRANDMA!"
+def speak_to_grandma(greeting)
+  if greeting == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif == *.upcase
+  elsif greeting == greeting.upcase
     puts "NO, NOT SINCE 1938!"
   else
       puts "HUH?! SPEAK UP, SONNY!"
