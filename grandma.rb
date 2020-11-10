2dec2c6811b6133a1e9c8f7622700bb4c4e8428e
David Barbour <danceguy303@gmail.com>
2020-11-10 20:07:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 
 def speak_to_grandma(phrase)
-  if phrase.downcase == true
+  if .downcase == true
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.upcase == true
+  elsif .upcase == true
     puts "NO, NOT SINCE 1938!"
   else
       puts "HUH?! SPEAK UP, SONNY!"
