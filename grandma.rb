1a610a8567f514238b7bbebb320aa0e026d1e571
David Barbour <danceguy303@gmail.com>
2020-11-10 20:19:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,19 +1,14 @@
 
-def speak_to_grandma()
+def speak_to_grandma
   if "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
+  elsif == .upcase
     puts "NO, NOT SINCE 1938!"
   else
       puts "HUH?! SPEAK UP, SONNY!"
     end
 end
 
-speak_to_grandma("I LOVE YOU GRANDMA!")
-speak_to_grandma("Hi, Nana, how are you?")
-speak_to_grandma("Hi!")
-speak_to_grandma("WHAT DI YOU EAT TODAY?")
-speak_to_grandma("WHAT?")
 
 
   # Write a speak_to_grandma method.
